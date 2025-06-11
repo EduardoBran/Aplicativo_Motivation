f654ff25a69fb0b2222cfb07f548ac2ea795ba16
EduardoBran <eduardo.ads1814@gmail.com>
2025-06-11 11:36:04 -0300

    preparando UserActivity para lidar com botão de salvar

--- app/src/main/java/com/luizeduardobrandao/motivation/UserActivity.kt
+++ app/src/main/java/com/luizeduardobrandao/motivation/UserActivity.kt
@@ -1,20 +1,44 @@
 package com.luizeduardobrandao.motivation
 
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.luizeduardobrandao.motivation.databinding.ActivityUserBinding
 
-class UserActivity : AppCompatActivity() {
+class UserActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var binding: ActivityUserBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_user)
+
+        binding = ActivityUserBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
+
+    override fun onClick(v: View) {
+        when (v.id) {
+            R.id.button_save -> {
+                handleButtonSave()
+            }
+        }
+    }
+
+    private fun handleButtonSave() {
+        // logicao ao clicar no botão
+    }
+
+    private fun setListeners(){
+        binding.buttonSave.setOnClickListener(this)
     }
 }
